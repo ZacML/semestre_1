fc74091b69545a3cfeabedb6391c2910379ef151
Isaque Lopes <isaquem673@gmail.com>
2025-03-31 21:38:22 -0300

    Update media02.py

--- media02.py
+++ media02.py
@@ -2,14 +2,14 @@ nota1 = float(input('Digite a priemira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 nota3 = float(input('Digite a terceira nota: '))
 
-aulas = int (input('Insira a quantidades de aulas: '))
-faltas = int(input('Insira a quantidade de faltas: '))
+qtdAulas = int (input('Insira a quantidades de aulas: '))
+qtdFaltas = int(input('Insira a quantidade de faltas: '))
 
 mediaNota = (nota1 + nota2 + nota3)/3
 
-presenca = (aulas - faltas)
+presenca = (qtdAulas - qtdFaltas)
 
-mediaPresenca = (presenca / aulas) * 100
+mediaPresenca = (presenca / qtdAulas) * 100
 
 if mediaNota >= 7 and mediaPresenca >= 75:
     print(f'Sua media é de: {mediaNota:.2f} \nSua presença é de: {mediaPresenca:.2f}% \nVocê foi APROVADO!!!')
@@ -17,3 +17,4 @@ elif mediaNota < 7 and mediaPresenca >= 75:
     print(f'Sua media é de: {mediaNota:.2f} \nSua presença é de: {mediaPresenca:.2f}% \nVocê esta em RECUPERAÇÃO!!!')
 else:
     print(f'Sua media é de: {mediaNota:.2f} \nSua presença é de: {mediaPresenca:.2f}% \nVocê foi REPROVADO!!!')
+
