8da6ef62640b4ff74346e89e3511fc2982c8d8a3
Isaque Lopes <isaquem673@gmail.com>
2025-04-10 19:32:14 -0300

    Delete identifica_vogal.py

--- identifica_vogal.py
+++
@@ -1,6 +0,0 @@
-letra = str(input('Digite uma letra para ver se é uma vogal(minúsculo): '))
-
-if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-    print('A letra digitada é uma vogal')
-else:
-    print('A letra digitada é uma consoante')
